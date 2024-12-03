b347019fb5eb727eac5a47a8f72e2825bdad3f8a
ice breaker <1324318532@qq.com>
2024-12-03 11:00:30 +0800

    chore: commit code

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
+import eruda from 'eruda'
 import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import './style.scss'
 import '@google/model-viewer'
 
+eruda.init()
+
 createApp(App).use(router).mount('#app')
+E
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,34 @@
 <script setup lang="ts">
+import type { ModelViewerElement } from '@google/model-viewer'
+import { onMounted, ref } from 'vue'
 
+const modelViewerRef0 = ref<ModelViewerElement>()
+const modelViewerRef1 = ref<ModelViewerElement>()
+
+onMounted(() => {
+  modelViewerRef1.value?.addEventListener('load', () => {
+    console.log('---------------load-start--------------------')
+    console.log(modelViewerRef0.value?.canActivateAR)
+    console.log(modelViewerRef1.value?.canActivateAR)
+    console.log('---------------load-end--------------------')
+  })
+  console.log('---------------onMounted-start--------------------')
+  console.log(modelViewerRef0.value?.canActivateAR)
+  console.log(modelViewerRef1.value?.canActivateAR)
+  console.log('---------------onMounted-end--------------------')
+})
 </script>
 
 <template>
   <div>
     <model-viewer
+      ref="modelViewerRef0"
       alt="xxxx"
       src="/NeilArmstrong.glb"
       ar shadow-intensity="1" camera-controls touch-action="pan-y"
     />
     <model-viewer
+      ref="modelViewerRef1"
       alt="xxxx"
       src="/1090012-3D.glb"
       ar shadow-intensity="1" camera-controls touch-action="pan-y"
