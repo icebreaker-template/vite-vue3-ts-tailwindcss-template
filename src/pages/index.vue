7bb363990d2132c241b23d3feec5fa0e67e33231
ice breaker <1324318532@qq.com>
2025-01-07 16:36:30 +0800

    chore(deps): upgrade

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,12 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+import axios from 'axios'
+import { ref } from 'vue'
+
+const uploadRef = ref<HTMLInputElement>()
+function onUpload() {
+  const files = uploadRef.value?.files
+  if (!files) {
+    return
+  }
+  const fd = new FormData()
+  fd.append('file', files[0])
+  axios.post('http://localhost:3000/upload', fd, {
+    onUploadProgress(progressEvent) {
+      console.log(progressEvent)
+    },
+  }).then((res) => {
+    console.log(res)
+  })
+}
 </script>
 
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <HelloWorld />
+  <div class="container mx-auto">
+    <input ref="uploadRef" type="file">
+    <button @click="onUpload">
+      Upload
+    </button>
   </div>
 </template>
