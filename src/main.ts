8b3c0160155c8fd6669515382b2d226519483bcb
ice breaker <1324318532@qq.com>
2025-01-15 14:04:32 +0800

    x

--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { router } from './router'
 import './style.scss'
+import 'floating-vue/dist/style.css'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+
+app.mount('#app')
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Dropdown, vClosePopper } from 'floating-vue'
 </script>
 
 <template>
@@ -7,6 +7,18 @@ import HelloWorld from '@/components/HelloWorld.vue'
     <h1 class="text-3xl font-bold underline">
       Hello world!
     </h1>
-    <HelloWorld />
+    <Dropdown
+      :distance="6"
+    >
+      <!-- This will be the popover reference (for the events and position) -->
+      <button>Click me</button>
+
+      <!-- This will be the content of the popover -->
+      <template #popper>
+        <div class="p-4">
+          <div>额sad sad sad sad爱上爱上爱上</div>
+        </div>
+      </template>
+    </Dropdown>
   </div>
 </template>
