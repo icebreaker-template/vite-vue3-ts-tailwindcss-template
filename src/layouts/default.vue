6be440a6370510cc4ad09b5b2fffbca4f33d471d
ice breaker <1324318532@qq.com>
2025-02-27 09:20:48 +0800

    chore: commit code

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { onMounted, useTemplateRef } from 'vue'
 import { RouterView } from 'vue-router'
+
+const container = useTemplateRef('container')
+
+onMounted(() => {
+  // container.value?.attachShadow({ mode: 'open' })
+  // if (container.value?.shadowRoot) {
+  //   container.value.shadowRoot.innerHTML = container.value.innerHTML
+  //   const style = document.createElement('style')
+
+  //   style.innerHTML = `
+  //   h1{
+  //     color: red;
+  //   }
+  //   `
+  //   container.value.shadowRoot.appendChild(style)
+  // }
+})
 </script>
 
 <template>
-  <div class="container mx-auto">
+  <div ref="container" class="container mx-auto">
     <RouterView />
   </div>
 </template>
