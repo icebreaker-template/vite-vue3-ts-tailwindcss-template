9776498e7fe89588bf12c6e2cc8a43b62498eb23
ice breaker <1324318532@qq.com>
2025-02-27 09:24:04 +0800

    chore: x

---
+++ src/components/HelloScopedWorld.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const { msg = 'icebreaker' } = defineProps<{
+  msg?: string
+}>()
+
+const name = defineModel<string>({
+  default: 'icebreaker',
+})
+</script>
+
+<template>
+  <div>
+    <h1> {{ msg }}</h1>
+    <div>Hello World</div>
+    <input v-model="name">
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: deeppink;
+}
+</style>
