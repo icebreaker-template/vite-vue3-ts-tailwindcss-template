79264fb6eb89a698dbb15433c4938f3dc0bc010d
ice breaker <1324318532@qq.com>
2025-03-10 15:19:10 +0800

    casc

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,53 +1,22 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
-import { toast } from '@/components/ui/toast'
+import { getCurrentInstance } from 'vue'
 
-import { toTypedSchema } from '@vee-validate/zod'
-import { useForm } from 'vee-validate'
-import { h } from 'vue'
-import * as z from 'zod'
+defineProps<{
+  type: string
+  dsa?: string
+}>()
 
-const formSchema = toTypedSchema(z.object({
-  username: z.string().min(2).max(50),
-}))
+const vm = getCurrentInstance()
 
-const { handleSubmit } = useForm({
-  validationSchema: formSchema,
-})
-
-const onSubmit = handleSubmit((values) => {
-  toast({
-    title: 'You submitted the following values:',
-    description: h('pre', { class: 'mt-2 w-[340px] rounded-md bg-slate-950 p-4' }, h('code', { class: 'text-white' }, JSON.stringify(values, null, 2))),
-  })
-})
+console.log(vm)
 </script>
 
 <template>
-  <form class="w-2/3 space-y-6" @submit="onSubmit">
-    <FormField v-slot="{ componentField }" name="username">
-      <FormItem>
-        <FormLabel>Username</FormLabel>
-        <FormControl>
-          <Input type="text" placeholder="shadcn" v-bind="componentField" />
-        </FormControl>
-        <FormDescription>
-          This is your public display name.
-        </FormDescription>
-        <FormMessage />
-      </FormItem>
-    </FormField>
-    <Button type="submit">
-      Submit
-    </Button>
-  </form>
+  <div>
+    <pre>
+      <code>
+        {{ vm.propsOptions }}
+      </code>
+    </pre>
+  </div>
 </template>
