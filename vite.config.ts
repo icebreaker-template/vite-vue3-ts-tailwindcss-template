f399db9b56ae24ee0c6116a3fe96f72882445d53
ice breaker <1324318532@qq.com>
2025-03-23 23:07:46 +0800

    x

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import Vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import Layouts from 'vite-plugin-vue-layouts'
+
+function r(p: string) {
+  return path.relative(__dirname, p)
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -22,6 +26,18 @@ export default defineConfig({
     VueI18nPlugin({
 
     }),
+    {
+      name: 'x',
+      resolveId(source, importer, options) {
+        console.log('resolveId', source)
+      },
+      load(id, options) {
+        console.log('load', r(id))
+      },
+      transform(code, id, options) {
+        console.log('transform', r(id))
+      },
+    },
   ],
   css: {
     preprocessorOptions: {
