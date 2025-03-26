305f065cc8affbea2d189ec45826514b2d2ccc1a
ice breaker <1324318532@qq.com>
2025-03-26 18:27:34 +0800

    chore(deps): upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import path from 'node:path'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import Tailwindcss from '@tailwindcss/vite'
 import Vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
 import Layouts from 'vite-plugin-vue-layouts'
@@ -23,12 +26,16 @@ export default defineConfig({
     VueI18nPlugin({
 
     }),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   css: {
     preprocessorOptions: {
-      scss: {
-
-      },
+      scss: { api: 'modern-compiler' },
     },
   },
 })
