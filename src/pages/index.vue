5ead2f6f8c3e8bef4691aeede000641436f0f85d
ice breaker <1324318532@qq.com>
2025-06-19 17:19:58 +0800

    chore(deps): upgrade

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,5 +3,35 @@
 </script>
 
 <template>
-  <div>1</div>
+  <div class="bg-amber-50 ppp">
+    ppp
+    <div class="aaa">
+      aaa
+    </div>
+  </div>
+  <div class="bg-blue-100 ppp sm">
+    ppp
+    <div class="aaa">
+      aaa
+    </div>
+  </div>
 </template>
+
+<style>
+.ppp {
+  color: red;
+
+  .aaa {
+    color: blue;
+    font-size: 50px;
+  }
+
+  & {
+    font-size: 100px;
+  }
+
+  &.sm {
+    font-size: 50px;
+  }
+}
+</style>
